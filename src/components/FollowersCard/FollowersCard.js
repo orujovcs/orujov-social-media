22a2b44b1647516a002f876f39db60fe6e2a6413
orujovcs <orujovcs@gmail.com>
2022-12-25 13:56:18 +0400

    add followers csrd

--- src/components/FollowersCard/FollowersCard.js
+++ src/components/FollowersCard/FollowersCard.js
@@ -1,39 +1,30 @@
-import React, { useEffect, useState } from "react";
-import "./followersCard.css";
-// import User from "../User/User";
-import { useSelector } from "react-redux";
+import React from 'react'
+import './followersCard.css'
+
+import { Followers } from '../Data/FollwersData'
 const FollowersCard = () => {
-//   const [modalOpened, setModalOpened] = useState(false);
-//   const [persons, setPersons] = useState([]);
-//   const { user } = useSelector((state) => state.authReducer.authData);
-
-//   useEffect(() => {
-//     const fetchPersons = async () => {
-//       const { data } = await getAllUser();
-//       setPersons(data);
-//     };
-//     fetchPersons();
-//   }, []);
-
   return (
     <div className="FollowersCard">
-      <h3>People you may know</h3>
+        <h3>Who is following you</h3>
 
-      {/* {persons.map((person, id) => {
-        if (person._id !== user._id) return <User person={person} key={id} />;
-      })}
-      {!location ? (
-        <span onClick={() => setModalOpened(true)}>Show more</span>
-      ) : (
-        ""
-      )} */}
-{/* 
-      <FollowersModal
-        modalOpened={modalOpened}
-        setModalOpened={setModalOpened}
-      /> */}
+        {Followers. map((follower, id)=>{
+            return(
+                <div className="follower">
+                    <div>
+                        <img src={follower.img} alt="" className='followerImage' />
+                        <div className="name">
+                            <span>{follower.name}</span>
+                            <span>@{follower.username}</span>
+                        </div>
+                    </div>
+                    <button className='button fc-button'>
+                        Follow
+                    </button>
+                </div>
+            )
+        })}
     </div>
-  );
-};
+  )
+}
 
-export default FollowersCard;
+export default FollowersCard
